bbecde40e0dc091bac52b03cce412f2c9af30799
judasaca2 <juandavid@vuedata.com>
2023-10-01 21:57:56 -0500

    Add cancel friendship invitation endpoint

--- src/routes/friendshipRoutes.ts
+++ src/routes/friendshipRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import authenticateToken from '../middlewares/securityMiddleware';
 import {
   acceptFriendshipInvitation,
+  cancelFriendshipInvitation,
   rejectFriendshipInvitation,
   sendFriendshipInvitation,
 } from '../services/friendshipInvitationServices';
@@ -41,6 +42,25 @@ router.post('/invitations/reject/:invitationId', (req, res) => {
   rejectFriendshipInvitation(receiverUsername, invitationId)
     .then(() => res.sendStatus(200))
     .catch(e => res.status(400).json({ message: e.message }));
+});
+
+router.post('/invitations/cancel/:invitationId', (req, res) => {
+  const invitationId = req.params.invitationId;
+  if (invitationId === null) {
+    res.status(400).json({
+      message: 'you are not giving the invitation id',
+    });
+  }
+  const senderUsername = req.body.verified_user.username;
+  cancelFriendshipInvitation(senderUsername, invitationId)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(() => {
+      res
+        .status(400)
+        .json({ message: 'You are not allowed to cancel this invitation.' });
+    });
 });
 
 export default router;
--- src/services/friendshipInvitationServices.ts
+++ src/services/friendshipInvitationServices.ts
@@ -111,3 +111,16 @@ export const rejectFriendshipInvitation = async (
     );
   }
 };
+
+export const cancelFriendshipInvitation = async (
+  senderUsername: string,
+  invitationId: string,
+): Promise<void> => {
+  await prisma.friendshipInvitation.delete({
+    where: {
+      senderUsername,
+      id: invitationId,
+      status: 'SENT',
+    },
+  });
+};
