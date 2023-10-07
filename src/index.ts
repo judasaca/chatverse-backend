7ee1a485dc311f6905a7322f45182d811f98ceec
judasaca2 <juandavid@vuedata.com>
2023-10-07 13:51:48 -0500

    Basic socket authentication

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import frienshipRoutes from './routes/friendshipRoutes';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import path from 'path';
+import jwt from 'jsonwebtoken';
+
 const app = express();
 app.use(express.json());
 app.use('/user', userRouter);
@@ -17,9 +19,47 @@ app.get('/', (_req, res) => {
   };
   res.sendFile('./index.html', options);
 });
+app.get('/1', (_req, res) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  res.sendFile('./client_test_1.html', options);
+});
+app.get('/2', (_req, res) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  res.sendFile('./client_test_2.html', options);
+});
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  if (token === null) {
+    next(new Error('No credentials'));
+  } else {
+    jwt.verify(
+      token,
+      process.env.TOKEN_SECRET as string,
+      (err: any, user: any) => {
+        if (err != null) {
+          next(new Error('Bad credentials'));
+          console.log('SOCKET: BAD CREDENTIALS');
+        } else {
+          socket.data.user_name = user.username;
+          console.log('SOCKET: SUCCESSFUL AUTHENTICATION ');
+          next();
+        }
+      },
+    );
+  }
+  // ...
+});
+// io.use((socket, next)=>{
+
+// })
 
 io.on('connection', socket => {
-  console.log('a user has connected');
+  console.log(socket.data);
+  console.log(`${socket.data.user_name} has connected`);
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
